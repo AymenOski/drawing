a4fe4694de98146aba6e29f35bcfedae5de170d6
AymenOski <aymenazizi610@gmail.com>
2026-01-09 14:32:21 +0100

    Add Point struct implementation with random generation and drawing capabilities

--- src/geometrical_shapes.rs
+++ src/geometrical_shapes.rs
@@ -1,3 +1,6 @@
+use rand::Rng;
+use raster::Color;
+
 // Traits
 pub trait Drawable {
     fn draw(&self, image: &mut impl Displayable);
@@ -35,4 +38,32 @@ pub struct Triangle {
 pub struct Circle {
     center: Point,
     radius: i32,
+}
+
+// Implementations
+
+impl Point {
+    pub fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+
+    pub fn random(max_width: i32, max_height: i32) -> Self {
+        let mut rng = rand::thread_rng();
+
+        Self {
+            x: rng.gen_range(0..max_width),
+            y: rng.gen_range(0..max_height),
+        }
+    }
+}
+
+impl Drawable for Point {
+    fn draw(&self, image: &mut impl Displayable) {
+        image.display(self.x, self.y, self.color());
+    }
+
+    fn color(&self) -> Color {
+        // Each shape must be drawn in a different color ; so I have chosen white for Point
+        Color::hex("#2600ffff").unwrap()
+    }
 }
